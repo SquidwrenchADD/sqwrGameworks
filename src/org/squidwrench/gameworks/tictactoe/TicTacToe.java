7cccf65ae1f419b084b17004ba009c907aef7ce0
Christopher Joslyn <christopher.joslyn@gmail.com>
2012-01-21 15:45:56 -0500

    Set up grid view with a dummy onclick event.

--- src/org/squidwrench/gameworks/tictactoe/TicTacToe.java
+++ src/org/squidwrench/gameworks/tictactoe/TicTacToe.java
@@ -3,6 +3,13 @@ package org.squidwrench.gameworks.tictactoe;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.view.View;
+
+import android.widget.GridView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+
 public class TicTacToe extends Activity
 {
     /** Called when the activity is first created. */
@@ -11,5 +18,13 @@ public class TicTacToe extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(TicTacToe.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
